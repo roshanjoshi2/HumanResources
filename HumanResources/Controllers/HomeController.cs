63af4c71553f5906a265f1786ee0fb2363e093df
Roshan Joshi <roshanjoshi224@gmail.com>
2022-12-31 18:51:02 +0545

    Add project files.

---
+++ HumanResources/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using HumanResources.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+
+namespace HumanResources.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Employee> employees = new List<Employee>();    
+        
+            string connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=HumanResources;Integrated Security=true";
+
+            // Provide the query string with a parameter placeholder.
+            string queryString = "select * from Employee";
+               
+
+            // Create and open the connection in a using block. This
+            // ensures that all resources will be closed and disposed
+            // when the code exits.
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var emp = new Employee
+                        {
+                            ID = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Address = reader.GetString(2),
+                            Gender = char.Parse(reader.GetString(3)),
+                            Dob = reader.GetDateTime(4)
+                        };
+                        employees.Add(emp); 
+                        
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+                return View(employees);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HumanResources/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace HumanResources.Models
+{
+    public class Employee
+    {
+
+        public int ID {  get; set; }
+
+        public string Name { get; set; }
+
+        public string? Address { get; set; }
+
+        //public string Email { get; set; }
+
+        public char Gender { get; set; }
+
+        public DateTime? Dob { get; set; }
+
+        //public DateTime JoinDate { get; set; }
+
+        //public string Department { get; set; }
+
+        //public string Designation { get; set; }
+    }
+}
